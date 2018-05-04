a60f8f55956528e62acc34b6a2bfd23d32af9cad
Tarquinc <tarquinc@go.stockton.edu>
2018-05-04 01:22:27 -0400

    Implemented interface; added "Users" controller (no implementation)

---
+++ Matricks/Data/UserRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+using Matricks.Models;
+
+namespace Matricks.Data
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly DataContext _context;
+
+        public UserRepository(DataContext context)
+        {
+            _context = context;
+        }
+
+        public void Add<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete<T>(T entity) where T : class
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<User> GetUser(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<User>> GetUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> SaveAll()
+        {
+            var save = await _context.SaveChangesAsync();
+            if (save > 0)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
