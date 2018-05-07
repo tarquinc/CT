b5d9bc289a1cee9ef53ec8a1a9249c6e5a8e239f
Tarquinc <tarquinc@go.stockton.edu>
2018-05-07 01:39:33 -0400

    Added a "GetAll" method to UsersController

--- Matricks/Controllers/UsersController.cs
+++ Matricks/Controllers/UsersController.cs
@@ -2,12 +2,17 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Matricks.Data;
+using Matricks.DTOs;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using AutoMapper;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace Matricks.Controllers
 {
+    [Authorize]
     public class UsersController : Controller
     {
         // GET: /<controller>/
@@ -15,5 +20,25 @@ namespace Matricks.Controllers
         {
             return View();
         }
+
+        public IMapper _userMap;
+
+        public IUserRepository _repository;
+
+        public UsersController(IUserRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        [Route("api/users/{id}")]
+        public async Task<IEnumerable<UserDTO>> GetAll(int id)
+        {
+            var allUsers = await _repository.GetUsers();
+            var info = _userMap.Map<IEnumerable<UserDTO>>(allUsers);
+            return info;
+        }
+
+        
     }
 }
---
+++ Matricks/DTOs/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿using Matricks.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matricks.DTOs
+{
+    public class UserDTO
+    {
+        public int ID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Gender { get; set; }
+        public string Name { get; set; }
+        public DateTime BirthDate { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string Introduction { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public ICollection<Photo> Photos { get; set; }
+    }
+}
