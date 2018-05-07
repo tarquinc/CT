b4100b4696ea4c7eee45078e9d71378f9f63d852
Tarquinc <tarquinc@go.stockton.edu>
2018-05-07 01:51:57 -0400

    Added BriefDTO

---
+++ Matricks/DTOs/BriefDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matricks.DTOs
+{
+    public class BriefDTO
+    {
+        public int ID { get; set; }
+        [Required]
+        public string UserName { get; set; }
+        public string Gender { get; set; }
+        public string Name { get; set; }
+        public DateTime Created { get; set; }
+        public DateTime LastActive { get; set; }
+        public string City { get; set; }
+        public int Age { get; set; }
+        public string Url { get; set; }
+    }
+}
