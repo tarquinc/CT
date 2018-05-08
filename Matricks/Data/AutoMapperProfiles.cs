38167d266781449536355ccea6423073c507a542
Tarquinc <tarquinc@go.stockton.edu>
2018-05-08 01:15:03 -0400

    Added class AutoMapperProfiles

---
+++ Matricks/Data/AutoMapperProfiles.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Matricks.DTOs;
+using Matricks.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Matricks.Data
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<User, BriefDTO>();
+            CreateMap<User, UserDTO>();
+        }
+    }
+}
